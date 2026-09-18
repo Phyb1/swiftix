bd0f623e9a0c4fc2d4cdf2a048557484291a8e6b
Phyb1 <phibeon@yahoo.com>
2026-09-18 11:37:07 +0200

    adding images to home

--- apps/core/views.py
+++ apps/core/views.py
@@ -8,16 +8,6 @@ from apps.services.models import Service
 class HomeView(TemplateView):
     template_name = "core/home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["featured_products"] = Product.objects.filter(
-            in_stock=True, is_featured=True
-        ).select_related("category")[:8]
-        context["categories"] = Category.objects.all()
-        context["services"] = Service.objects.filter(is_active=True)
-        context["recent_posters"] = Poster.objects.filter(is_active=True)[:4]
-        return context
-
-
+   
 class AboutView(TemplateView):
     template_name = "core/about.html"
